c870a1f4d3c7c6587e0df71873c7736b828c7d2e
Luís Gustavo Oliveira da Silva <luis.gustavo.ods@gmail.com>
2024-10-24 09:26:43 -0300

    Add project files.

---
+++ AssyncTasks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace FinanceBot
+{
+    internal class AssyncTasks
+    {
+
+        public static void Print_One()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Hello from Print 1");
+                Task.Delay(50).Wait();
+            }
+            Console.WriteLine("ENDED PRINT 1");
+
+        }
+
+        public static void Print_Two()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Hello from Print 2");
+                Task.Delay(50).Wait();
+            }
+
+            Console.WriteLine("ENDED PRINT 2");
+
+        }
+
+      
+
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using FinanceBot;
+using System;
+using System.Threading.Tasks;
+
+
+class Program {
+
+
+    static void Main(string[] args) {
+
+        Console.WriteLine("*******************************************************");
+
+        Console.WriteLine("SYNCHRONOUS");
+
+        AssyncTasks.Print_One();
+        AssyncTasks.Print_Two();
+
+
+        Console.WriteLine("*******************************************************");
+        Console.WriteLine("ASSYNCHRONOUS");
+
+        Task task1 = Task.Run(() => AssyncTasks.Print_One());
+        Task task2 = Task.Run(() => AssyncTasks.Print_Two());
+
+        Task.WaitAll(task1, task2);
+
+
+        Console.WriteLine("*******************************************************");
+
+
+
+    }
+
+
+    
+
+
+}
